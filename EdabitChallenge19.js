26122dc18b16958a5b20f7f0f12e20c889501122
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-06-21 16:23:39 +0100

    Function to check if inputted date is 24th Dec.

---
+++ EdabitChallenge19.js
@@ -0,0 +1,6 @@
+//Christmas Eve is almost upon us, so naturally we need to prepare some milk and cookies for Santa! Create a function that accepts a Date object and returns true if it's Christmas Eve (December 24th) and false otherwise. Keep in mind JavaScript's Date month is 0 based, meaning December is the 11th month while January is 0.
+
+function timeForMilkAndCookies(date) {
+	return date.getMonth() === 11 && date.getDate() === 24;
+}
+	
