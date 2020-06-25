27f97823063dfcb8bcfb0245fd18ab100d35aa53
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-06-25 14:26:08 +0100

    Returns 0 if input is 1, returns 1 if input is 0.

---
+++ EdabitChallenge26.js
@@ -0,0 +1,12 @@
+// Write a function that returns 0 if the input is 1, and returns 1 if the input is 0. Try completing this challenge without using any:
+
+//Conditionals
+//Ternary operators
+//Negations
+//Bit operators
+
+function flip(y) {
+	var x = y - 1;
+	return (Math.abs(x))
+	
+}
