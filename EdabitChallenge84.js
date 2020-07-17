e3ba42157fb65c643b80f979bad24241b14f2914
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-07-17 20:10:51 +0100

    Native Promise, Introducing the Executor.

---
+++ EdabitChallenge84.js
@@ -0,0 +1,9 @@
+//Promises are just objects that contain the outcome of asynchronous operations. So when do you use one? When you want to control the outcome of an asynchronous operation. All you have to do is wrap the asynchronous function with a promise constructor.
+
+//The promise constructor requires you to pass a function called the executor which takes two parameters, resolve and reject. Both are functions that you use to pass or reject a value that is usually the result of the async operation. Here's an example of a simple promise:
+
+let promise = new Promise( (resolve, reject) => {
+  setTimeout(( ) => {
+     resolve("edabit")
+  }, 1000)
+})
