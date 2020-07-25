9ca06aca5a1e77c88c4afba70e15bed831ed8845
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-07-26 00:37:26 +0100

    RegEx Exercise: An empty string

---
+++ EdabitChallenge106.js
@@ -0,0 +1,3 @@
+//If you've completed this RegEx series from I to XXII then you have been exposed to all of MDN's documentation on regular expressions special characters. You can check my Collections under Basic Reg Ex in my profile if you missed any. This next part of the series is to help solidify what you've learned. In order to save time I will be searching the web to find regular expression exercises to post here.
+
+const REGEXP = /^(?=)$/
