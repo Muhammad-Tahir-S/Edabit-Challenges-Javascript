19ece25568666acb5c7c6afd8744a7ce352e0b88
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-08-01 23:23:32 +0100

    Make a Circle with OOP

---
+++ EdabitChallenge122.js
@@ -0,0 +1,15 @@
+//Your task is to create a Circle constructor that creates a circle with a radius provided by an argument. The circles constructed must have two getters getArea() (PIr^2) and getPerimeter() (2PI*r) which give both respective areas and perimeter (circumference).
+
+class Circle {
+  constructor (radius) {
+    this.radius = radius;
+  }
+
+  getArea() {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+
+  getPerimeter() {
+    return 2 * Math.PI * this.radius;
+  }
+}
