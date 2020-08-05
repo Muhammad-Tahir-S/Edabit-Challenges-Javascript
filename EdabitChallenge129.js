6367b7893182d6681350a1450038fd01eba89a8d
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-08-05 19:50:58 +0100

    Hitting the Jackpot

---
+++ EdabitChallenge129.js
@@ -0,0 +1,5 @@
+//Create a function that takes in a number as a string n and returns the number without trailing and leading zeros.
+
+function testJackpot(result) {
+	return new Set(result).size === 1
+}
