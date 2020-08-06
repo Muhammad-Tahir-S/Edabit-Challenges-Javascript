fa35e3d0e2c46ffe5798d0c3fa0d4278b02b94b3
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-08-06 23:34:09 +0100

    Volume of a Cone

---
+++ EdabitChallenge137.js
@@ -0,0 +1,3 @@
+//Create a function that takes the height and radius of a cone as arguments and returns the volume of the cone rounded to the nearest hundredth.
+
+const coneVolume=(h, r)=>+(Math.PI*r*r*h/3).toFixed(2)
