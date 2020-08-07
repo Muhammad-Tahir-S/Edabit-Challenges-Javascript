990d52d8d8d0e217156727841e1d4fd30d0d1b62
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-08-07 22:27:24 +0100

    Count Ones in Binary Representation of Integer

---
+++ EdabitChallenge138.js
@@ -0,0 +1,3 @@
+//Count the amount of ones in the binary representation of an integer. So for example, since 12 is '1100' in binary, the return value should be 2.
+
+const coneVolume=(h, r)=>+(Math.PI*r*r*h/3).toFixed(2)
