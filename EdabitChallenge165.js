f7a265d7380ff73dc95307dbb9e7fa11eac57510
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-09-10 22:31:05 +0100

    RegEx: Character Classes XI ⁠- \S

---
+++ EdabitChallenge165.js
@@ -0,0 +1,3 @@
+//Write a regex that will return true if the bio does not have any spaces before the last ending punctuation ?. You must use the \S character class in your expression.
+
+const REGEXP = /\w+\S\?/g
