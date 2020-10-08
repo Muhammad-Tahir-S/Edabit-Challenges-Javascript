89d682117aab3b89dd539b53dd0aad475ef84906
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-10-08 20:20:06 +0100

    Find the Discount

---
+++ EdabitChallenge218.js
@@ -0,0 +1,5 @@
+//Create a function that takes two arguments: the final price and the discount as integers and returns the final price after the discount.
+
+function dis(price, discount) {
+	 return price * (100 - discount) * 0.01
+}
