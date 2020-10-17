881cd5ad122155944cb1d3d18e2ee5a850c5e737
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-10-17 21:39:05 +0100

    ES6: Destructuring Arrays V

---
+++ EdabitChallenge248.js
@@ -0,0 +1,3 @@
+//ES6 Destructuring can come in handy when you use regular expressions. Here is a function that uses a regular expression to parse a URL.
+
+const str = `[,protocol, host, path] = parsedURL
