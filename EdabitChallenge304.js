eea7d721a366582811d8e614623b19d562b06ad1
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2020-11-21 06:53:00 +0100

    Is the Die Loaded

---
+++ EdabitChallenge304.js
@@ -0,0 +1,5 @@
+//Given an array with the six observed frequencies, write a function that returns true if a die is unloaded, or false if it is loaded. Take 11.0705 as the critical value for all cases.
+
+function derivative(b, m) {
+	return b * m ** (b - 1)
+}
