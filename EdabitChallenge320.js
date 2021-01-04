406bf96d90650fe280d763def1cd67666a197f0a
Muhammad-Tahir Sanuth <65069483+Muhammad-Tahir-S@users.noreply.github.com>
2021-01-04 06:45:02 +0100

    Consecutive Sum Check

---
+++ EdabitChallenge320.js
@@ -0,0 +1,3 @@
+//Create a function that takes a number n as an argument and checks whether the given number can be expressed as a sum of two or more consecutive positive numbers.
+
+let consecutiveSum =n=> Math.log2(n) % 1 > 0;
